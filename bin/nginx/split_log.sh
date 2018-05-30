2e89b5866ec82403395baf65ccad2c397d5b3359
wxnacy <371032668@qq.com>
2018-05-30 13:11:05 +0000

    update nginx split log

--- bin/nginx/split_log.sh
+++ bin/nginx/split_log.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
 NGINX_HOME=/usr/local/openresty/nginx
-LOG_DIR=${NGINX_HOME}/logs
 LOG=access.log,error.log
+LOG_DIR=${NGINX_HOME}/logs
 
-# sudo touch ${LOG_DIR}/${LOG_API}
-# sudo touch ${LOG_DIR}/${LOG_HOST}
 
 DT=`date "+%Y%m%d-%H%M%S"`
 
-sudo test -d ${LOG_DIR}/api || sudo mkdir ${LOG_DIR}/api
-sudo test -d ${LOG_DIR}/host || sudo mkdir ${LOG_DIR}/host
-
-# 检查日志文件是否存在
-sudo test -f ${LOG_DIR}/${LOG_API} \
-    && sudo mv ${LOG_DIR}/${LOG_API} ${LOG_DIR}/api/${LOG_API}.${DT}
-sudo test -f ${LOG_DIR}/${LOG_HOST} \
-    && sudo mv ${LOG_DIR}/${LOG_HOST} ${LOG_DIR}/host/${LOG_HOST}.${DT}
-
 LOGS=(${LOG//,/ })
-echo $LOGS
+echo ${LOGS[@]}
+
+for f in ${LOGS[@]};do
+    dir=${LOG_DIR}/${f//./_}
+    test -d $dir || sudo mkdir -p $dir
+    test -f ${LOG_DIR}/$f && sudo mv ${LOG_DIR}/$f ${dir}/$f.${DT}
+done
 
 sudo ${NGINX_HOME}/sbin/nginx -s reopen
