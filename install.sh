6c72e817db32d6eb343a161f0e7bc0c8eeba961c
wxnacy <371032668@qq.com>
2018-05-30 15:26:20 +0800

    add

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+WSHELL_HOME=${HOME}/.wshell
+
+test -d ${WSHELL_HOME} || git clone https://github.com/wxnacy/wshell ${WSHELL_HOME}
+
+
+if [[ ! $PATH =~ '.wshell/bin' ]];then
+
+    echo 'WARNING: seems you still have not added 'wshell' to the load path.'
+    echo ''
+    echo '# Load pyenv automatically by adding'
+    echo '# the following to ~/.bash_profile:'
+    echo 'export PATH="${HOME}/.wshell/bin:$PATH"'
+fi
--- push.sh
+++ push.sh
@@ -2,4 +2,6 @@
 
 MSG=$1
 
+cp bin/shell.sh bin/shell
+
 git pull && git add . && git commit -m "${MSG}" && git push origin master
